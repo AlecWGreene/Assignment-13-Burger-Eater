476eb22fe15772fb6e63ad5da66c940c77681000
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-09-07 13:12:03 -0400

    Implemented a login and register feature so that users can register to the database, and then login pinging the database to check

---
+++ public/assets/js/login-script.js
@@ -0,0 +1,44 @@
+// REGISTRATION METHODS
+// ============================================================
+
+function registerHandler(arg_event){
+    // Prevent default
+    arg_event.preventDefault();
+    console.log("Event data: ", arg_event.data);
+
+    // Register user
+    $.ajax({
+        url: "/api-login/register",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(arg_event.data),
+        success: function(arg_data){
+            console.log("AJAX data: ", arg_data);
+        }
+    });
+}
+
+// LOGIN METHODS
+// ============================================================
+
+// Handle login attempt
+function loginHandler(arg_event){
+
+    // Prevent event's default
+    arg_event.preventDefault();
+    console.log("Event data: ", arg_event.data);
+    
+    // Send login request
+    $.ajax({
+        url: "/api-login/login",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(arg_event.data)
+    });
+}
+
+// Add event listeners
+$("#login-form").on("submit", { username: $("#input-username").val(), password: $("#input-password").val() }, loginHandler);
+$("#register-form").on("submit", { username: $("#register-username").val(), password: $("#register-password").val(), display: $("#register-display").val() }, registerHandler);
--- server.js
+++ server.js
@@ -6,23 +6,45 @@ const expHB = require("express-handlebars");
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+// Setup passport
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      });
+    }
+  ));
 
 // Load scripts
-const loginRouter = require("");
+const loginRouter = require("./lib/controllers/login");
+const pageController = require("./lib/controllers/page-controller");
 
 // Initialize express 
 const app = express();
 
 // Configure express
 const PORT = process.env.PORT || 8080;
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(routes); // Normally after handlebars
 
 // Configure handlebars 
 app.engine("handlebars", expHB({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Setup routes
+app.use(loginRouter);
+app.use(pageController);
 
 // Initialize application
 app.listen(PORT, function() {
