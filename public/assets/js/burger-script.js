b6506188e9700fa7fe18c8390e57740802a2e6a8
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-09-14 12:09:38 -0400

    Finished implementation of rendering methods

--- public/assets/js/burger-script.js
+++ public/assets/js/burger-script.js
@@ -1,14 +1,24 @@
-let ingredients = [];
-let renderedIngredients = [];
 let currentIndex = 0;
 
 // Runtime
 $(document).ready(() => {
     // Add event listeners
     $(".burger-option").on("click", ingredientButtonHandler);
+    $("#view-btn").on("click", viewButtonHandler);
 });
 
+// Display burger based on selection
+function viewButtonHandler(arg_event){
+    let ingredients = [];
 
+    // Assemble selected ingredients
+    for(let t_ingredient of $(".burger-option.active")){
+        ingredients.push($(t_ingredient).text().trim());
+    }
+
+    // Call render method
+    renderBurger(ingredients);
+}
 
 // Assemble the button selections
 function ingredientButtonHandler(arg_event){
@@ -17,7 +27,6 @@ function ingredientButtonHandler(arg_event){
 
     // Get button text
     let t_text = $(arg_event.target).text().trim();
-    console.log(t_text);
 
     // If it's a bun only allow one to be selected
     if(["Plain","Sesame","Pumpernickel"].includes(t_text)){
@@ -53,31 +62,112 @@ function ingredientButtonHandler(arg_event){
     }
 }
 
-function renderBurger(arg_ingredients){
-    // Render each ingredient
-    for(let i = 0; i < arg_ingredients.length; i++){
-        renderIngredient(t_ingredient);
+function renderBurger(arg_ingredients){ 
+    clearBurger();
+
+    // Get bun selection
+    if(arg_ingredients.includes("Plain")){
+        renderIngredient("Plain_Bottom");
+    }
+    else if(arg_ingredients.includes("Pumpernickel")){
+        renderIngredient("Pumpernickel_Bottom");
+    }
+    else if(arg_ingredients.includes("Sesame")){
+        renderIngredient("Sesame_Bottom");
+    }
+
+    // Render patties
+    if(arg_ingredients.includes("Single")){
+        renderIngredient("Patty");
+    }
+    if(arg_ingredients.includes("Double")){
+        renderIngredient("Patty");
+        renderIngredient("Patty");
+    }
+    if(arg_ingredients.includes("Triple")){
+        renderIngredient("Patty");
+        renderIngredient("Patty");
+        renderIngredient("Patty");
+    }
+
+    // Sauce
+    if(arg_ingredients.includes("Ketchup")){
+        renderIngredient("Ketchup");
+    }
+    if(arg_ingredients.includes("Mustard")){
+        renderIngredient("Mustard");
+    }
+
+    // Ingredients
+    if(arg_ingredients.includes("Cheese")){
+        renderIngredient("Cheese");
+    }
+    if(arg_ingredients.includes("Lettuce")){
+        renderIngredient("Lettuce");
+    }
+    if(arg_ingredients.includes("Tomato")){
+        renderIngredient("Tomato");
+    }
+    if(arg_ingredients.includes("Onions")){
+        renderIngredient("Onions");
+    }
+    if(arg_ingredients.includes("Bacon")){
+        renderIngredient("Bacon");
+    }
+    if(arg_ingredients.includes("Egg")){
+        renderIngredient("Egg");
+    }
+    if(arg_ingredients.includes("Pickles")){
+        renderIngredient("Pickles");
+    }
+
+    // Finish the burger
+    if(arg_ingredients.includes("Plain")){
+        renderIngredient("Plain_Top");
+    }
+    else if(arg_ingredients.includes("Pumpernickel")){
+        renderIngredient("Pumpernickel_Top");
+    }
+    else if(arg_ingredients.includes("Sesame")){
+        renderIngredient("Sesame_Top");
     }
 }
 
-async function renderIngredient(arg_ingredient){
+// Remove current displayed ingredients
+function clearBurger(){
+    $(".burger-ingredient").remove();
+}
+
+function renderIngredient(arg_ingredient){
     let $img = $("<img>").addClass("burger-ingredient").css("display","none");
     
     switch(arg_ingredient){
         case "Plain_Bottom":
-            $img.attr("src","Burger_Bottom_Bun.png").css("margin-bottom", "-200px").css("z-index","1");
+            $img.attr("src","Burger_Bottom_Bun.png").css("margin-bottom", "-160px").css("z-index","1");
             currentIndex = 2;
             break;
         case "Pumpernickel_Bottom":
-            $img.attr("src","Burger_Bottom_Bun_Pumpernickel.png").css("margin-bottom", "-200px").css("z-index","1");
+            $img.attr("src","Burger_Bottom_Bun_Pumpernickel.png").css("margin-bottom", "-160px").css("z-index","1");
             currentIndex = 2;
             break;
         case "Sesame_Bottom":
-            $img.attr("src","Burger_Bottom_Bun_Sesame.png").css("margin-bottom", "-200px").css("z-index","1");
+            $img.attr("src","Burger_Bottom_Bun_Sesame.png").css("margin-bottom", "-160px").css("z-index","1");
+            currentIndex = 2;
+            break;
+        case "Plain_Top":
+            $img.attr("src","Burger_Top_Bun.png").css("margin-bottom", "-120px").css("z-index","17");
+            currentIndex = 2;
+            break;
+        case "Pumpernickel_Top":
+            $img.attr("src","Burger_Top_Bun_Pumpernickel.png").css("margin-bottom", "-120px").css("z-index","17");
+            currentIndex = 2;
+            break;
+        case "Sesame_Top":
+            $img.attr("src","Burger_Top_Bun_Sesame.png").css("margin-bottom", "-120px").css("z-index","17");
             currentIndex = 2;
             break;
         case "Patty":
-            $img.attr("src","Patty.png").css("margin-bottom", "-100px").css("z-index",currentIndex);
+            $img.attr("src","Patty.png").css("margin-bottom", "-115px").css("z-index",currentIndex);
             currentIndex++;
             break;
         case  "Cheese":
@@ -93,7 +183,7 @@ async function renderIngredient(arg_ingredient){
             currentIndex++;
             break;
         case "Onions":
-            $img.attr("src","Onions.png").css("margin-bottom", "-136px").css("z-index",currentIndex);
+            $img.attr("src","Onions.png").css("margin-bottom", "-125px").css("margin-top","30px").css("z-index",currentIndex);
             currentIndex++;
             break;
         case "Bacon":
@@ -105,7 +195,7 @@ async function renderIngredient(arg_ingredient){
             currentIndex++;
             break;
         case "Pickles":
-            $img.attr("src","Pickles.png").css("margin-bottom", "-156px").css("z-index",currentIndex);
+            $img.attr("src","Pickles.png").css("margin-bottom", "-135px").css("margin-top","-30px").css("z-index",currentIndex);
             currentIndex++;
             break;
         case "Ketchup":
@@ -118,17 +208,11 @@ async function renderIngredient(arg_ingredient){
             break;
     }
 
-    // Display ingerdient at top of div
+    // Display ingerdient
     $(".burger-slider").prepend($img);
     $img.offset({top: 0 });
     $img.css("display","block");
 
-    // Signal end of animation
-    renderedIngredients.push(arg_ingredient);
     return;
 }
 
-function sortIngredients(){
-    let t_array = [];
-
-}
